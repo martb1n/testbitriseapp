971f1578b593f5aeec6836a63b3427967be063c0
Alexander Martinevsky <a.martinevsky@hotmail.com>
2016-01-06 23:46:15 +0300

    app is running

--- App2/App2/App.cs
+++ App2/App2/App.cs
@@ -12,17 +12,7 @@ namespace App2
 		public App ()
 		{
 			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+		    MainPage = new TestPage {BindingContext = new SimpleViewModel()};
 		}
 
 		protected override void OnStart ()
---
+++ App2/App2/SimpleViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public class SimpleViewModel : INotifyPropertyChanged
+    {
+        public SimpleViewModel()
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(1), ()  =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    MainText = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+                });
+
+                return true;
+            });
+        }
+
+        private string _mainText;
+
+        public string MainText
+        {
+            get { return _mainText; }
+            set
+            {
+                _mainText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
